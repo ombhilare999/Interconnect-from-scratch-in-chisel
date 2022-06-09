eeccefc4a1d262ecedc79a77c5ca9eaa3475d9d2
omkar bhilare <ombhilare999@gmail.com>
2022-06-09 17:40:03 +0200

    Updated the FSM modules and test

--- src/main/scala/fsm_comm.scala
+++ src/main/scala/fsm_comm.scala
@@ -7,101 +7,112 @@ import chisel3.experimental.ChiselEnum
 /*
 Transmitter  <------->   Receiver
     FSM                    FSM
-1) Generating Clock Signal
-2) Write Signal from the transmitter
-3) 
-*/
+ */
+object Main extends App () {
 
-
-    
-    class Transmitter extends Module () {
-        val io = IO (
-            new Bundle {
-                val START = Input(UInt(0.W)) //Triggers the communication
-                val WR = Output(UInt(0.W))
-                val RD = Output(UInt(0.W))
-                val ADD = Output(UInt(32.W))
-                val WDATA = Output(UInt(32.W))
-                val RDATA = Input(UInt(32.W))
-            }
-        )
-
-        object State extends ChiselEnum {
-            val sNone, sOne, sTwo, sThree = Value
+    class Transmitter extends Module() {
+    val io = IO(
+        new Bundle {
+        val START = Input(UInt(0.W)) // Triggers the communication
+        val WR = Output(UInt(0.W))
+        val RD = Output(UInt(0.W))
+        val ADD = Output(UInt(32.W))
+        val WDATA = Output(UInt(32.W))
+        val RDATA = Input(UInt(32.W))
         }
+    )
 
-        val state = RegInit(State.sNone)
-
-        switch(state) {
-            is(State.sNone) {
-                when(io.START === 1.U) {
-                    state := State.sOne
-                }
-            }
-            is(State.sOne) {   //Write Operation First Cycle
-                io.WR := 1.U     //Sending Write as 1 
-                io.RD := 0.U     //Keeping Read signal de-asserted
-                io.ADD := 10.U   //Lower Bit is 10
-                io.WDATA := 20.U //Data to be write at location 10 is 20
-                state := State.sTwo
-            }
-            is(State.sTwo) {
-                io.ADD := 11.U   //Address Changes to 11
-                io.WDATA := 40.U //Data changes to 40
-                state := State.sThree
-            }
-            is(State.sThree) { //Deasserting Everything
-                io.WR := 0.U
-                io.RD := 0.U
-                io.ADD := 0.U
-                io.WDATA := 0.U 
-                state := State.sNone
-            }
-        }
+    object State extends ChiselEnum {
+        val sNone, sOne, sTwo, sThree = Value
     }
 
-    class Receiver extends Module () {
-        val io = IO (
-            new Bundle {
-                val WR = Input(UInt(0.W))
-                val RD = Input(UInt(0.W))
-                val ADD = Input(UInt(32.W))
-                val WDATA = Input(UInt(32.W))
-                val RDATA = Output(UInt(32.W))
-                val CDATA = Output(UInt(32.W))
-            }
-        )        
+    val state = RegInit(State.sNone)
 
-        object State extends ChiselEnum {
-            val sNone, sOne, sTwo, sThree = Value
+    switch(state) {
+        is(State.sNone) {
+        when(io.START === 1.U) {
+            state := State.sOne
         }
-
-        val state = RegInit(State.sNone)
-
-        switch(state) {
-            is(State.sNone) {
-                when(io.WR  === 1.U) {   //Right now Supporting Write Operation Only
-                    state := State.sOne
-                }
-            }
-            is(State.sOne) {   //Write Operation First Cycle
-                when(io.WR === 1.U) {
-                    when(io.ADD === 10.U) {
-                        io.CDATA := io.WDATA //Storing the write data into current data
-                        state := State.sTwo
-                    }
-                }
-            }
-            is(State.sTwo) {
-                when(io.WR === 1.U) {
-                    when(io.ADD === 11.U) {
-                        io.CDATA := io.WDATA //Storing the write data into current data
-                        state := State.sThree
-                    }
-                }
-            }
-            is(State.sThree) { //Deasserting Everything
-                io.CDATA := 0.U
-            }
+        }
+        is(State.sOne) { // Write Operation First Cycle
+        io.WR := 1.U // Sending Write as 1
+        io.RD := 0.U // Keeping Read signal de-asserted
+        io.ADD := 10.U // Lower Bit is 10
+        io.WDATA := 20.U // Data to be write at location 10 is 20
+        state := State.sTwo
+        }
+        is(State.sTwo) {
+        io.ADD := 11.U // Address Changes to 11
+        io.WDATA := 40.U // Data changes to 40
+        state := State.sThree
+        }
+        is(State.sThree) { // Deasserting Everything
+        io.WR := 0.U
+        io.RD := 0.U
+        io.ADD := 0.U
+        io.WDATA := 0.U
+        state := State.sNone
         }
     }
+    }
+
+    class Receiver extends Module() {
+    val io = IO(
+        new Bundle {
+        val WR = Input(UInt(0.W))
+        val RD = Input(UInt(0.W))
+        val ADD = Input(UInt(32.W))
+        val WDATA = Input(UInt(32.W))
+        val RDATA = Output(UInt(32.W))
+        val CDATA = Output(UInt(32.W))
+        }
+    )
+
+    object State extends ChiselEnum {
+        val sNone, sOne, sTwo, sThree = Value
+    }
+
+    val state = RegInit(State.sNone)
+
+    switch(state) {
+        is(State.sNone) {
+        when(io.WR === 1.U) { // Right now Supporting Write Operation Only
+            state := State.sOne
+        }
+        }
+        is(State.sOne) { // Write Operation First Cycle
+        when(io.WR === 1.U) {      val WR = Output(UInt(0.W))
+        val RD = Output(UInt(0.W))
+        val ADD = Output(UInt(32.W))
+        val WDATA = Output(UInt(32.W))
+        val RDATA = Input(UInt(32.W))
+            }
+        }
+        is(State.sTwo) {
+        when(io.WR === 1.U) {
+            when(io.ADD === 11.U) {
+            io.CDATA := io.WDATA // Storing the write data into current data
+            state := State.sThree
+            }
+        }
+        }
+        is(State.sThree) { // Deasserting Everything
+        io.CDATA := 0.U
+        }
+    }
+    }
+
+    class Top(val start: UInt) extends Module {
+
+        val Tx = Module(new Transmitter)
+        val Rx = Module(new Receiver)
+
+        //Connecting both the modules
+        Tx.io.WR := Rx.io.WR
+        Tx.io.RD := Rx.io.WR
+        Tx.io.ADD := Rx.io.WR
+        Tx.io.WDATA := Rx.io.WR
+        Tx.io.RDATA := Rx.io.WR
+        Tx.io.START := start
+    }
+}
--- src/test/scala/fsm_test.scala
+++ src/test/scala/fsm_test.scala
@@ -1,19 +1,13 @@
 package fsm_test
 
 import chisel3._
-import chiseltest._
-import chiseltest.ChiselScalatestTester
-import org.scalatest.flatspec.AnyFlatSpec
-import org.scalatest.matchers.should.Matchers
+import chisel3.tester._
+import chisel3.tester.RawTester.test
 
-class fsm_test extends AnyFlatSpec with ChiselScalatestTester {
-    test(new fsm_comm.Transmitter) { c =>
+object Main extends App () { 
 
+    test(new fsm_comm.Top(start = 1.U)) { c =>
+        step(3)
     }
 
-    test(new fsm_comm.Receiver ) { d =>
-
-    }
-
-    println("Hello World")
 }
